35207772705d46c7424025f9052916662de2abe5
chengxuyuan-nq <3399618442@qq.com>
2021-01-22 22:30:54 +0800

    1_22

---
+++ test_1_22/test_1_22/test.c
@@ -0,0 +1,48 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	char arr[] = { 'a', 'b', 'c' };
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+//int Fun(int n)
+//{
+//	if (n == 5)
+//		return 2;
+//	else
+//		return 2 * Fun(n + 1);
+//}
+//
+//int main()
+//{
+//	printf("%d", Fun(2));
+//	return 0;
+//}
+
+
+//递归算法--汉诺塔
+//int Hanoi(int n)
+//{
+//	if (n == 1)
+//	{
+//		return 1;
+//	}
+//	return 2 * Hanoi(n - 1) + 1;
+//}
+//
+//
+//int main()
+//{
+//	int n = 0;//a柱有n个盘子
+//	scanf("%d", &n);
+//	int ret=Hanoi(n);
+//	printf("所需次数：%d\n", ret);
+//	return 0;
+//}
+//
+
+
