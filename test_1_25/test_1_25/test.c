9eddf1f6daf195323058c593e43ede4ac8373861
chengxuyuan-nq <3399618442@qq.com>
2021-01-25 20:26:59 +0800

    代码

---
+++ test_1_25/test_1_25/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	a = 5;
+//	c = ++a;
+//	b = ++c, c++, ++a, a++;
+//	//b += a++ + c;
+//	printf("a = %d b = %d c = %d\n:", a, b, c);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	a = a^b;
+//	b = b^a;
+//	a = a^b;
+//	printf("a=%d,b=%d\n", a, b);
+//	return 0;
+//}
