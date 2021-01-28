40a6a243424b95a0516951ee0d87920b0a1e2aa9
chengxuyuan-nq <3399618442@qq.com>
2021-01-28 22:46:42 +0800

    练习代码

---
+++ test_1_28/test_1_28/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+
+//Nice公司面试题
+
+//int main()
+//{
+//	int i = 0;
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hello bit\n");//死循环
+//	}
+//	return 0;
+//}
+
