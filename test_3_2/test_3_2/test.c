5d928e33dfef14a79ab7c7b019172780e2239dca
chengxuyuan-nq <3399618442@qq.com>
2021-03-02 23:18:09 +0800

    练习代码

---
+++ test_3_2/test_3_2/test.c
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string>
+//#include<iostream>
+//using namespace std;
+
+//int main()
+//{
+//	if ((strlen("abc") - strlen("abcdef")) > 0)
+//		cout << "大于" << endl;
+//	else
+//		cout << "小于" << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[20] = "abcdefgh******";
+//	char* p = "hello world";
+//	strcpy(arr, p);
+//	cout << arr << endl;
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char arr[] = "abcde";
+//	char p[] = "abde";
+//	cout << strcmp(arr,p) << endl;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[20] = "abcde ";
+//	char p[] = "hello";
+//	cout << strcat(arr, p) << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "hello, how are you ?";
+//	char p[] = "are";
+//	cout << strstr(arr, p) << endl;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char str[] = "hello,how are you ?";
+//	char * p;
+//	p = strtok(str, ",?");
+//	while (p != NULL)
+//	{
+//		printf("%s\n", p);
+//		p = strtok(NULL, ",?");
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<string.h>
+//
+////模拟实现strlen
+//int main()
+//{
+//
+//	return 0;
+//}
+//
+
+
+
