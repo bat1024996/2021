94e43f063b29cb1c0df7d43efbd7fee8af552c9a
bat1024996 <3399618442@qq.com>
2021-04-24 22:52:11 +0800

    test.4.24

---
+++ test4_23/test4_23/test.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//int main()
+//{
+//	vector<int> arr;
+//	for (int i = 0; i < 10; ++i)
+//	{
+//		arr.push_back(i);
+//	}
+//	for (auto num : arr)
+//	{
+//		cout << num << " ";
+//	}
+//	cout << endl;
+//	for (auto &ret : arr)
+//	{
+//		ret = 0;
+//	}
+//	for (auto num : arr)
+//	{
+//		cout << num << " ";
+//	}
+//	cout << endl;
+//
+//	return 0;
+//}
+
+
+//void test1()
+//{
+//	int sz = 4;
+//	int *b = new int[sz];
+//	for (int i = 0; i < sz; ++i)
+//	{
+//		b[i] = i;
+//	}
+//	delete []b;     //释放数组的空间
+//}
+//
+//void test2()
+//{
+//	int sz = 3;
+//	int **c = new int*[sz]; //创建二维数组
+//	for (int i = 0; i < 3; ++i)
+//	{
+//		c[i] = new int[5];
+//		for (int j = 0; j < 5; ++j)
+//		{
+//			c[i][j] = j;
+//		}
+//	}
+//	for (int i = 0; i < 3; ++i)
+//	{
+//		delete[]c[i];
+//	}
+//}
+//
+//int main()
+//{
+//	test1();
+//	test2();
+//	return 0;
+//}
+
+//template <class T>
+
+
